97c25d194125ad5b6e45d28253d723967e04670a
Sam <parkssa123@gmail.com>
2023-02-27 11:32:35 -0700

    udpates

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -2,14 +2,19 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
 #include <firexcode/firexcode_plugin.h>
 #include <image_editor_pro/image_editor_pro_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FirexcodePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirexcodePlugin"));
   ImageEditorProPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ImageEditorProPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
 }
